33b16fcf91ebe53e554788e244cc4f507d241979
RubenCruchaga <rc103008@wgcloud.org>
2023-11-17 10:59:22 -0600

    fixewd the bug(no more while loop)

--- cooking.py
+++ cooking.py
@@ -5,55 +5,44 @@ This cood will figer out how long it will take to cook your turcky useing what t
 turkyNum=int(input("How maney turckey are you cooking: "))
 for i in range (turkyNum):
     turcky=1
-    #ask what type of cooking
+#ask what type of cooking
     typeOfCook=input("How do you what to cook your turcky (oven roasted or boilled, grilled, smoked or fryed): ")
-    #ask how may pound the turky is
+#ask how may pound the turky is
     pound=float(input("How heavy is your turck(in pounds): "))
-    # lets the user do it agen
-    while turkyNum == turcky:
-        #breaks off the user to the two forms of cooking that the program lest
-        if typeOfCook == "oven roasted":
-            # the 14 is the avegr cooking time per pound
-            pound=pound*14
-            print("it will take "+ str(pound)+ " minets")
-            
-        #this is for boilled
-        elif typeOfCook == "boilled":
-            #takes ruffly 3.75 minnets per pound of bird
-            pound=pound*3.75
-            print("when the oil is ready it will take "+str(pound)+" minets")
-            
+# lets the user do it agen
+    #breaks off the user to the two forms of cooking that the program lest
+    if typeOfCook == "oven roasted":
+        # the 14 is the avegr cooking time per pound
+        pound=pound*14
+        
+    #this is for boilled
+    elif typeOfCook == "boilled":
+        #takes ruffly 3.75 minnets per pound of bird
+        pound=pound*3.75
 
-        elif typeOfCook == "grilled":
-            #takes ruffly 12 minnets per pound of bird
-            pound=pound*12
-            print("when the grille is ready it will take "+str(pound)+" minets")
-            
+    elif typeOfCook == "grilled":
+        #takes ruffly 12 minnets per pound of bird
+        pound=pound*12
 
-        elif typeOfCook == "smoked":
-            #takes ruffly 35 minets per pound of bird
-            pound=pound*35
-            print("when the smoker is ready it will take "+str(pound)+" minets")
-            
+    elif typeOfCook == "smoked":
+        #takes ruffly 35 minets per pound of bird
+        pound=pound*35
 
-        elif typeOfCook == "fryed":
-            #takes ruffly 3.5 minnets per pound of bird
-            pound=pound*3.5
-            print("when the fryer is ready it will take "+str(pound)+" minets")
-            
+    elif typeOfCook == "fryed":
+        #takes ruffly 3.5 minnets per pound of bird
+        pound=pound*3.5
 
-        #lets the user to use a differt type of cooking
-        else:
-            print("I do not know how to cook a turcky like that. sorry")
-            
-            #ask the user if you need stop or continu
-            fin=input("Are you done or not(yes,no)")
-            if fin == "no":
-                break
-            #ask the question agen
-            typeOfCook=input("How do you what to cook your turcky (oven roasted or boilled): ")
-            pound=float(input("How heavy is your turck(in pounds): "))
-
+    #lets the user to use a differt type of cooking
+    else:
+        print("I do not know how to cook a turcky like that. sorry")
+        
+        #ask the user if you need stop or continu
+        fin=input("Are you done or not(yes,no)")
+        if fin == "no":
+            break
+        #ask the question agen
+        typeOfCook=input("How do you what to cook your turcky (oven roasted or boilled): ")
+        pound=float(input("How heavy is your turck(in pounds): "))
     print(str(pound)+" minets, for your "+str(turcky)+ " turcky")
     #this keeps the  pound information
     totalPound=0
